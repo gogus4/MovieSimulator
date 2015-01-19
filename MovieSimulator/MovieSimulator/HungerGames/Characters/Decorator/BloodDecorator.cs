25a45adcaa1faa322b9064ce6ab63e1e9cbdcc20
Vladk-el <eliott.laversin@gmail.com>
2015-01-19 22:37:23 +0100

    bug git visual

---
+++ MovieSimulator/MovieSimulator/HungerGames/Characters/Decorator/BloodDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieSimulator.Common.BoardGame.Characters.Decorator;
+
+namespace MovieSimulator.HungerGames.Characters.Decorator
+{
+    class BloodDecorator : DecoratorAbstract
+    {
+        public override string DoMyReport()
+        {
+            return base.DoMyReport() + ", je suis couvert de sang";
+        }
+    }
+}
