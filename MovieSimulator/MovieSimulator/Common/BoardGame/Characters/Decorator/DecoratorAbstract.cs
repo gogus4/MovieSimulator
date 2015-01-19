d7895977d6eef9416dab80119b73d669596b0c44
Vladk-el <eliott.laversin@gmail.com>
2015-01-19 22:37:49 +0100

    re bub visual git

---
+++ MovieSimulator/MovieSimulator/Common/BoardGame/Characters/Decorator/DecoratorAbstract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSimulator.Common.BoardGame.Characters.Decorator
+{
+    public abstract class DecoratorAbstract : ComponentDecoratorAbstract
+    {
+        protected ComponentDecoratorAbstract component;
+
+        public void SetComponent(ComponentDecoratorAbstract imbriquedComponent)
+        {
+            component = imbriquedComponent;
+        }
+
+        public override string DoMyReport()
+        {
+            if (component != null)
+            {
+                return component.DoMyReport();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
