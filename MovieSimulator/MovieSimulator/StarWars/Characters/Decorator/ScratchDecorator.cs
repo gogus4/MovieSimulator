9c065d667f01acff4d4b8ae9f0e26654a1d366fe
Vladk-el <eliott.laversin@gmail.com>
2015-01-27 16:44:05 +0100

    add star wars comportements

---
+++ MovieSimulator/MovieSimulator/StarWars/Characters/Decorator/ScratchDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieSimulator.Common.BoardGame.Characters.Decorator;
+
+namespace MovieSimulator.StarWars.Characters.Decorator
+{
+    class ScratchDecorator : DecoratorAbstract
+    {
+        public override string DoMyReport()
+        {
+            return base.DoMyReport() + ", mon vaisseau est couvert d'égratinures";
+        }
+    }
+}
